97cf81104529a58403ffb921312695603171dcdc
Christian Johannes <cajiky@gmail.com>
2018-10-06 01:49:22 -0500

    FINISHED ALL

--- client.js
+++ client.js
@@ -1,11 +1,7 @@
 //checking sourcing
 let employeeArray = [];
-console.log('JS');
-$(nowReady);
-function nowReady(){
-    console.log('JQ');
-    $('#submitButton').on('click', submitNewEmployee);
-}
+
+//creating a new class called Employee that I will use to store the user inputs into. 
 class Employee{
     constructor(firstName, lastName, id, title, annualSalary){
         this.firstName = firstName;
@@ -16,23 +12,38 @@ class Employee{
     }
 }
 
-function submitNewEmployee(event) {
-    event.preventDefault();
-    console.log('submit button click');
-    let firstName = $('#firstName').val();
-    let lastName = $('#lastName').val();
-    let id = $('#id').val();
-    let title = $('#title').val();
-    let annualSalary = $('#annualSalary').val();
-    let freshEmployee = new Employee(firstName,lastName,id,title,annualSalary);
-    employeeArray.push(freshEmployee);
-    console.log(employeeArray);
+console.log('JS');
+$(nowReady);
+function nowReady(){
+    console.log('JQ');
+    $('#submitButton').on('click', submitNewEmployee);
+    $('#employeeTable').on('click','.deleteEmployee', deleteEmployee);
+}
+
+
+
+//function that occurs on cliq ;)
+function submitNewEmployee() {
+    createNewEmployee();
     clearInputFields();
     appendEmloyeeArray();
     calculateTotalSalary();
 }
 
+//function to create a new employee :)
+function createNewEmployee(){
+    //assigning js values to user inputs
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let id = $('#id').val();
+    let title = $('#title').val();
+    let annualSalary = $('#annualSalary').val();
+    //build a new Employee using values assigned ^^
+    let freshEmployee = new Employee(firstName,lastName,id,title,annualSalary);
+    employeeArray.push(freshEmployee);
+}
 
+//function only used to clear the text fields to prep for the next "submitNewEmployee"
 function clearInputFields() {
     $('#firstName').val('');
     $('#lastName').val('');
@@ -45,19 +56,43 @@ function appendEmloyeeArray() {
     let element = $('#employeeTable')
     element.empty();
     for(let employee of employeeArray){
-        element.append(`<tr><td>${employee.firstName}</td><td>${employee.lastName}</td><td>${employee.id}</td><td>${employee.title}</td><td>${employee.annualSalary}`)
+        element.append(`<tr class="tableRow"><td>${employee.firstName}</td><td>${employee.lastName}</td><td>${employee.id}</td><td>${employee.title}</td><td>$${Number(employee.annualSalary).toLocaleString('en')}<button class="deleteEmployee" style="font-size:12px;margin:0px; ">Delete</button></td></tr>`)
     }
 }
-
+let monthlyCostStr;
 function calculateTotalSalary(){
     let accumulatedSalary = 0;
     for(let employee of employeeArray){
+        //replaces any commas or $ inputed by user and turns into num
         let cleanNum = employee.annualSalary.replace('$','');
         cleanNum = parseInt(cleanNum.replace(',',''));
         accumulatedSalary += cleanNum
     }
     let monthlyCostNum = accumulatedSalary/12;
+    //rounds to two decimal places.
+    monthlyCostNum = Math.round(monthlyCostNum*100)/100;
+    //adds Commas to numbers
     let monthlyCostStr = Number(monthlyCostNum).toLocaleString('en');
-    console.log(monthlyCostStr);
+    //Time to append the new "monthlyCostStr" to the DOM
+    $('#finalSalaryOutput').empty();
+    $('#finalSalaryOutput').append(`<h2>Total Cost: $${monthlyCostStr}`)
+}
+
+//Creating a function to remove an Employee from the DOM & Array
+function deleteEmployee(){
+    console.log('delet button clicked')
+    let employeeHtmlLine = $(this).parentsUntil($('.centeredText')).text();
+
+    for(i = 0; i<employeeArray.length; i++){
+        if(employeeHtmlLine.toString === employeeArray[i].id.toString){
+            console.log('Succ found an ID that Matches');
+            employeeArray.splice(i,1);
+            $(this).parentsUntil($('.centeredText')).empty();
+            calculateTotalSalary();
+            return
+        }
+        console.log('couldnt find matching ID')
+    }
+    
 }
 
