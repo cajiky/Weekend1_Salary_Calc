72d440c43d92e924257f101b4e3536ad9f7bc5a0
Christian Johannes <cajiky@gmail.com>
2018-10-05 21:58:19 -0500

    finished building the table and the append thingy

--- client.js
+++ client.js
@@ -7,12 +7,12 @@ function nowReady(){
     $('#submitButton').on('click', submitNewEmployee);
 }
 class Employee{
-    constructor(firstName, lastName, id, title, anualSalary){
+    constructor(firstName, lastName, id, title, annualSalary){
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
         this.title = title;
-        this.anualSalary = anualSalary;
+        this.annualSalary = annualSalary;
     }
 }
 
@@ -23,18 +23,28 @@ function submitNewEmployee(event) {
     let lastName = $('#lastName').val();
     let id = $('#id').val();
     let title = $('#title').val();
-    let anualSalary = $('#anualSalary').val();
-    let freshEmployee = new Employee(firstName,lastName,id,title,anualSalary);
+    let annualSalary = $('#annualSalary').val();
+    let freshEmployee = new Employee(firstName,lastName,id,title,annualSalary);
     employeeArray.push(freshEmployee);
     console.log(employeeArray);
+    clearInputFields();
+    appendEmloyeeArray();
+}
+
+
+function clearInputFields() {
     $('#firstName').val('');
     $('#lastName').val('');
     $('#id').val('');
     $('#title').val('');
-    $('#anualSalary').val('');
+    $('#annualSalary').val('');
 }
 
 function appendEmloyeeArray() {
-    let element = $()
+    let element = $('#employeeTable')
+    element.empty();
+    for(let employee of employeeArray){
+        element.append(`<tr><td>${employee.firstName}</td><td>${employee.lastName}</td><td>${employee.id}</td><td>${employee.title}</td><td>${employee.anualSalary}`)
+    }
 }
 
