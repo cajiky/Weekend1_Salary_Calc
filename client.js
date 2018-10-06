0158629466cdf4e6fb33a74439a75ecccf2036dc
Christian Johannes <cajiky@gmail.com>
2018-10-05 19:12:33 -0500

    Just started working @ WOG

--- client.js
+++ client.js
@@ -3,4 +3,10 @@ console.log('JS');
 $(nowReady);
 function nowReady(){
     console.log('JQ');
+    $('#submit').on('click', submitNewEmployee);
 }
+function submitNewEmployee(event) {
+    event.preventDefault();
+    console.log('submit button click');
+}
+
